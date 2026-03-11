9d5d3548758322910c117a50a51578815a647d8a
thvnn116 <th.vnn116@gmail.com>
2026-03-11 09:59:59 +0700

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -16,19 +16,25 @@ document.getElementById("empShift").innerText = user.shift;
 
 async function checkin(){
 
+const user = JSON.parse(localStorage.getItem("currentUser"));
+
 const url = "https://script.google.com/macros/s/AKfycbw-hlUyOgU_eerQnykqBLRbz7Tfrn1U9AJnhnInqGQBU1FAuFtnNKyKXQTkPVuxP0jh/exec";
 
 const query =
 "?action=checkin" +
 "&username=" + user.username +
-"&password=" + user.password +
 "&deviceId=" + user.deviceId +
 "&shift=" + user.shift;
 
-fetch(url + query,{
+await fetch(url + query,{
 method:"GET",
 mode:"no-cors"
 });
+
+const btn = document.getElementById("checkinBtn");
+
+btn.innerText = "Đã chấm công";
+btn.disabled = true;
 
 alert("Chấm công thành công");
 
